22e4b91af7819a1cc4d97e6722c81dac202096f0
raphisky <raphael.kammoun@gmail.com>
2019-01-18 18:54:32 +0100

    Initial commit

---
+++ rct.js
@@ -0,0 +1,38 @@
+
+var reactionTimeResult;
+var startingClickTimeStamp;
+var finalClickTimeStamp;
+var gameIsOn = false;
+
+$('#actionButton').click( function( event ){
+
+  gameIsOn = true;
+  startingClickTimeStamp = event.timeStamp;
+  console.log("starting click = " +startingClickTimeStamp);
+  testReactionTime(startingClickTimeStamp);
+
+});
+
+function spawnReactionSignal() {
+  $('#reactionZone').css('background-color','blue');
+}
+
+var triggerReaction;
+
+function testReactionTime(u) {
+    var randomDelay = Math.floor((Math.random() * 4000) + 2000);
+    console.log("random delay = " + randomDelay);
+    triggerReaction = setTimeout(spawnReactionSignal, randomDelay);
+
+    $('#reactionZone').click( function( event ){
+
+          if (gameIsOn) {
+            finalClickTimeStamp = event.timeStamp;
+            console.log("second click : " + finalClickTimeStamp);
+            reactionTimeResult = finalClickTimeStamp - startingClickTimeStamp - randomDelay;
+            console.log("reaction time = " + reactionTimeResult);
+            gameIsOn = false;  $('#reactionZone').css('background-color','pink');
+            return reactionTimeResult;
+          }
+    });
+};
